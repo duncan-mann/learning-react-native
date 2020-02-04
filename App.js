0d882efcf6890136fc61f770c583a24457d9c7d8
Duncan <duncan.s.mann@gmail.com>
2020-02-04 15:20:25 -0500

    Add state to background color, and add button to change background to color to blue onPress

--- App.js
+++ App.js
@@ -4,20 +4,25 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
 
   const [text, changeText] = useState('Open up App.js to start working on your app!');
+  const [backgroundColor, changeBackgroundColor] = useState('#fff');
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: backgroundColor,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
 
   return (
     <View style={styles.container}>
       <Text>{text}</Text>
       <Button title="Change Text" onPress={() => changeText('New Text!') }/>
+      <Button title="Change Background" onPress={ () => changeBackgroundColor('blue')} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
