6f834f3b73bbb16d2282fd47f44a60f6f04185c5
Duncan <duncan.s.mann@gmail.com>
2020-02-04 15:15:02 -0500

    Add state to main text which changes on button press

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [text, changeText] = useState('Open up App.js to start working on your app!');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Change Text"/>
+      <Text>{text}</Text>
+      <Button title="Change Text" onPress={() => changeText('New Text!') }/>
     </View>
   );
 }
