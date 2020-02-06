a360fdebdf52fa0ca374baea6ae063e5822f039b
Duncan <duncan.s.mann@gmail.com>
2020-02-05 19:36:38 -0500

    Importing modal and wrapping contents of input in Modal component

--- components/Input.js
+++ components/Input.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Modal } from 'react-native'
 
 const Input = (props) => {
     return (
+      <Modal>
     <View style={styles.inputView}>
         <TextInput placeholder="Enter a Goal" style={styles.textInput} onChangeText={props.setGoal} value={props.toDo} />
         <Button title="Add" onPress={props.addGoal} />
       </View>
+      </Modal>
     )
 }
 
